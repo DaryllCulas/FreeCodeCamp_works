46c4d0142aa25c91e0cdaed3a0b7eb5b7291e4b5
DaryllCulas <culasdaryll99@gmail.com>
2023-05-11 01:49:54 +0800

    Add comparisons-with-the-logical-and-operator.js

---
+++ Javascript_DSA/comparisons-with-the-logical-and-operator.js
@@ -0,0 +1,15 @@
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+if(val >= 25 && val <= 50) {
+  return "Yes";
+}
+else {
+  return "No";
+}
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
