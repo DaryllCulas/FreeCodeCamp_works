009cbce722c7dec2676848b0bdd450b9d07886d0
DaryllCulas <culasdaryll99@gmail.com>
2023-05-11 01:02:49 +0800

    Add stand-in-line2.js

---
+++ Javascript_DSA/stand-in-line2.js
@@ -0,0 +1,17 @@
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item);
+  const removedArr = arr.shift(item);
+// return arr.shift(item);
+  return removedArr;
+  // Only change code above this line
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+let testArr2 =[5, 6, 7, 8, 9];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
