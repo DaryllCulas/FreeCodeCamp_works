180e7d8adf5cb7aa3b503b0c263314483c4ff222
DaryllCulas <culasdaryll99@gmail.com>
2023-05-11 02:58:01 +0800

    Modified stand-in-line.js

--- Javascript_DSA/stand-in-line.js
+++ Javascript_DSA/stand-in-line.js
@@ -17,7 +17,7 @@ function nextInLine(arr, item) {
 
 // Setup
 let testArr = [1, 2, 3, 4, 5];
-let testArr2 =[5, 6, 7, 8, 9];
+
 
 // Display code
 console.log("Before: " + JSON.stringify(testArr));
