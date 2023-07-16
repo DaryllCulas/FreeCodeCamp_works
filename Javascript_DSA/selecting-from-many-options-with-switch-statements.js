0d55c664dff3c9f43119caab1fc502210514c11d
DaryllCulas <culasdaryll99@gmail.com>
2023-07-17 02:45:09 +0800

    Add selecting-from-many-options-with-switch-statements.js

--- Javascript_DSA/selecting-from-many-options-with-switch-statements.js
+++ Javascript_DSA/selecting-from-many-options-with-switch-statements.js
@@ -1,19 +1,19 @@
 function caseInSwitch(val) {
   let answer = "";
   // Only change code below this line
-  switch(val) {
-     case 1:   
-      answer = "alpha";
-        break;
-    case 2:
-      answer = "beta";
-        break;
-    case 3: 
-      answer = "gamma";
-        break;
-    case 4:
-      answer = "delta";
-  }
+switch(val) {
+  case 1: 
+    return "alpha";
+      break;
+  case 2:
+    return "beta";
+      break;
+  case 3: 
+    return "gamma";
+      break;
+  case 4:
+    return "delta";
+}
 
 
   // Only change code above this line
