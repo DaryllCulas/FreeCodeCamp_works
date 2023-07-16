c163fea2a2a092801f52bb168a4d3dbfbc6440b9
DaryllCulas <culasdaryll99@gmail.com>
2023-07-17 01:37:45 +0800

    Add introducing-else-if-statements.js

--- Javascript_DSA/introducing-else-if-statements.js
+++ Javascript_DSA/introducing-else-if-statements.js
@@ -3,14 +3,12 @@ function testElseIf(val) {
     return "Greater than 10";
   }
 
-  else if (val < 5) {
+ else if (val < 5) {
     return "Smaller than 5";
   }
-  else {
-    return "Between 5 and 10";
+else {
+  return "Between 5 and 10";
   }
-
-  
 }
 
 testElseIf(7);
