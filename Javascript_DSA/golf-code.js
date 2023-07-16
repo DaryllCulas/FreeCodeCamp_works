401de243fe0f14853e9ed7e2e9200836209abb74
DaryllCulas <culasdaryll99@gmail.com>
2023-07-17 02:41:44 +0800

    Add golf-code.js

--- Javascript_DSA/golf-code.js
+++ Javascript_DSA/golf-code.js
@@ -2,35 +2,42 @@ const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey"
 
 function golfScore(par, strokes) {
   // Only change code below this line
-if(strokes === 1) {
-    par = names[0]; // Hole-in-one
-      return par;
+
+
+if(strokes == 1) {
+  par = names[0];
+ 
 }
 else if(strokes <= par-2) {
-    par = names[1]; // Eagle
-      return par;
+   par = names[1];
+ 
 }
 else if(strokes == par-1) {
-    par = names[2]; // Birdie
-      return par;
+   par = names[2];
+  
+
 }
-else if(strokes === par) {
-     par = names[3]; // Par
-      return par;
+else if(strokes == par) {
+   par = names[3];
+  
+
 }
 else if(strokes == par + 1) {
-    par = names[names.length-3]; // Bogey
-      return par;
+   par = names[4];
+ 
+ 
 }
 else if(strokes == par + 2) {
-    par = names[names.length-2]; // Double Bogey
-      return par;
+   par = names[5];
+ 
 }
 else if(strokes >= par + 3) {
-    par = names[names.length-1]; // Go Home
-      return par;
-}
+  par = names[6];
  
+ 
+}
+
+  return par;
   // Only change code above this line
 }
 
