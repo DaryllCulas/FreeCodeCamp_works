252ee916cf92d22e207ec57fcb1b87fb332b0b51
DaryllCulas <culasdaryll99@gmail.com>
2023-07-17 00:09:39 +0800

    Add use-conditional-logic-with-if-statements.js

--- Javascript_DSA/use-conditional-logic-with-if-statements.js
+++ Javascript_DSA/use-conditional-logic-with-if-statements.js
@@ -3,11 +3,9 @@ function trueOrFalse(wasThatTrue) {
   if(wasThatTrue) {
     return "Yes, that was true";
   }
-  else {
-    return "No, that was false"
-  }
+  return "No, that was false";
 
 
   // Only change code above this line
-  
+
 }
