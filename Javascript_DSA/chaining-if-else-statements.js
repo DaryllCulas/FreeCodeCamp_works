2702d50d96a53b41ecbb364794d7c6ea07ce79b1
DaryllCulas <culasdaryll99@gmail.com>
2023-07-26 00:41:39 +0800

    Modified chaining-if-else-statements.js

--- Javascript_DSA/chaining-if-else-statements.js
+++ Javascript_DSA/chaining-if-else-statements.js
@@ -1,23 +1,24 @@
 function testSize(num) {
   // Only change code below this line
-if(num < 5) {
-    return "Tiny";
-}
-else if(num < 10) {
-    return "Small";
-}
-else if(num < 15) {
-    return "Medium"
-}
-else if(num < 20) {
-    return "Large";
-}
-else if(num >= 20) {
-    return "Huge";
-}
-else {
-    return "Change Me";
+let size = "";
+
+  if(num < 5) {
+    size = "Tiny";
   }
+  else if(num < 10) {
+    size = "Small";
+  }
+  else if(num < 15) {
+    size = "Medium";
+  }
+  else if(num < 20) {
+    size =  "Large";
+  }
+  else if(num >= 20) {
+    size = "Huge";
+  }
+
+  return size;
   // Only change code above this line
 }
 
