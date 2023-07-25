4818605df4a4c3d3cfd36fcf9941c185051a4c09
DaryllCulas <culasdaryll99@gmail.com>
2023-07-26 01:56:33 +0800

    Add accessing-object-properties-with-dot-notation.js

--- Javascript_DSA/accessing-object-properties-with-dot-notation.js
+++ Javascript_DSA/accessing-object-properties-with-dot-notation.js
@@ -6,5 +6,5 @@ const testObj = {
 };
 
 // Only change code below this line
-const hatValue = testObj.hat;      // Change this line
-const shirtValue = testObj.shirt;    // Change this line
+const hatValue = testObj.hat;     
+const shirtValue = testObj.shirt;    
