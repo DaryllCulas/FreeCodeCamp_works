924544eb8c6ba06d0d744275bf3d891b0d26e95a
DaryllCulas <culasdaryll99@gmail.com>
2023-07-27 02:00:27 +0800

    Add manipulating-complex-objects.js

---
+++ Javascript_DSA/manipulating-complex-objects.js
@@ -0,0 +1,25 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Michael Jackson",
+    "title": "Beat it",
+    "release_year": 2002,
+    "formats": [
+      "CD",
+      "Casette",
+      "Diskette"
+    ],
+    "gold": false
+  }
+
+];
