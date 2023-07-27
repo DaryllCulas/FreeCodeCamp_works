d2e8b9ba08804f7f137c1c440f39ac7929e5a66b
DaryllCulas <culasdaryll99@gmail.com>
2023-07-28 01:03:33 +0800

    Add iterate-with-javascript-do---while-loops.js

---
+++ Javascript_DSA/iterate-with-javascript-do---while-loops.js
@@ -0,0 +1,15 @@
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+// while (i < 5) {
+//   myArray.push(i);
+//   i++;
+// }
+
+do {
+  myArray.push(i);
+  i++;
+
+}while(i < 5);
