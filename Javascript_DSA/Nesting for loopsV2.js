ca1db72d14e86e9672ad3a1f9c4ab36dded144de
DaryllCulas <culasdaryll99@gmail.com>
2023-07-28 00:55:56 +0800

    Add Nesting for loopsV2.js

---
+++ Javascript_DSA/Nesting for loopsV2.js
@@ -0,0 +1,14 @@
+const arr = [[1,2],[3,4],[5,6,7]];
+
+for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr.length; j++) {
+        console.log(arr[i][j]);
+    }
+}
+
+
+// for(let i = 0; i < arr.length; i++) {
+//     for(let j = 0; j < arr[i].length; j++) {
+//         console.log(arr[i][j]);
+//     }
+// }
