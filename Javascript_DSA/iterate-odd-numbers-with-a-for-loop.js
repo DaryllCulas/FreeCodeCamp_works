c3d51300027cb8e2fe706d7e195f7003a1a28edd
DaryllCulas <culasdaryll99@gmail.com>
2023-07-27 20:16:11 +0800

    Add iterate-odd-numbers-with-a-for-loop.js

---
+++ Javascript_DSA/iterate-odd-numbers-with-a-for-loop.js
@@ -0,0 +1,11 @@
+// Setup
+const myArray = [];
+
+// Only change code below this line
+// for(let i = 1; i <= 9; i += 2) {
+//   myArray.push(i);
+// }
+
+for(let i = 1; i <= 9; i = i + 2) {
+  myArray.push(i);
+}
