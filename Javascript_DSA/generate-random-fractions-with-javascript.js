54311a2bca5ea38d846f05ec74bed945dde3ace6
DaryllCulas <culasdaryll99@gmail.com>
2023-07-29 00:35:10 +0800

    Add generate-random-fractions-with-javascript.js

---
+++ Javascript_DSA/generate-random-fractions-with-javascript.js
@@ -0,0 +1,15 @@
+function randomFraction() {
+
+  // Only change code below this line
+  let randomGeneratedNum = 0;
+
+  while(randomGeneratedNum === 0) {
+    randomGeneratedNum = Math.random();
+  }
+    return randomGeneratedNum;
+
+
+  // Only change code above this line
+}
+
+randomFraction();
