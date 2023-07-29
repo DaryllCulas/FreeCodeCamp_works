4dfe1d9adb3a6da4fb9863ab2e5f79f72590d7b9
DaryllCulas <culasdaryll99@gmail.com>
2023-07-30 02:09:05 +0800

    Add golf-codev2.js

---
+++ Javascript_DSA/golf-codev2.js
@@ -0,0 +1,29 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+if(strokes === 1) {
+  return names[0];
+}
+else if(strokes <= par - 2) {
+  return names[1];
+}
+else if(strokes === par - 1) {
+  return names[2];
+}
+else if(strokes === par) {
+  return names[3];
+}
+else if(strokes === par + 1) {
+  return names[names.length-3];
+}
+else if(strokes === par + 2) {
+  return names[names.length-2];
+}
+else if(strokes >= par + 3) {
+  return names[names.length-1];
+}
+  // Only change code above this line
+}
+
+golfScore(5, 4);
