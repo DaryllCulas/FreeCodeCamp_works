8396a6fd3ef9d27caff3af054b1bb522185c198b
DaryllCulas <culasdaryll99@gmail.com>
2023-08-07 02:49:28 +0800

    Add use-destructuring-assignment-with-the-rest-parameter-to-reassign-array-elements.js

---
+++ Javascript_DSA/use-destructuring-assignment-with-the-rest-parameter-to-reassign-array-elements.js
@@ -0,0 +1,9 @@
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [ n1, n2, ...shorterList] = list; // Change this line
+  // Only change code above this line
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
