76c8283532a7ce5aaecea1a937844afb8f271ed5
DaryllCulas <culasdaryll99@gmail.com>
2023-08-12 03:24:32 +0800

    Add use-class-syntax-to-define-a-constructor-functionV2.js

---
+++ Javascript_DSA/use-class-syntax-to-define-a-constructor-functionV2.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+  veggie() {
+    return `this.name`;
+  }
+
+}
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
