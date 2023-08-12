e1e8442c00d25a1cd47646c91a0d64d2279fd39d
DaryllCulas <culasdaryll99@gmail.com>
2023-08-13 02:28:44 +0800

    Add complete-a-promise-with-resolve-and-reject.js

---
+++ Javascript_DSA/complete-a-promise-with-resolve-and-reject.js
@@ -0,0 +1,15 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    // Change this line
+    resolve("We got the data");
+
+  } else {  
+    // Change this line
+    reject("Data not received");
+  }
+});
+
+console.log(makeServerRequest);
