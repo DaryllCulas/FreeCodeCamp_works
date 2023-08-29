7db1ad1bf829be19b60f4528c8cd2f5a706fd9af
DaryllCulas <culasdaryll99@gmail.com>
2023-08-30 01:56:16 +0800

    Add Fibonacci Sequence.js

---
+++ Javascript_DSA/Fibonacci Sequence.js
@@ -0,0 +1,13 @@
+
+const fibonacci = (n) => {
+    const fib = [0,1];
+        for(let i = 2; i < n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];               
+        }
+    return fib;
+   
+}
+
+console.log(fibonacci(7));
+
+//Big-0 = 0(n) 
