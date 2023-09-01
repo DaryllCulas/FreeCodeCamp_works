3df54b16a65daaf4e1ff6c3ece97c41afd31bbc1
DaryllCulas <culasdaryll99@gmail.com>
2023-09-02 02:38:30 +0800

    Add check-for-the-presence-of-an-element-with-indexof.js

--- Javascript_DSA/check-for-the-presence-of-an-element-with-indexof.js
+++ Javascript_DSA/check-for-the-presence-of-an-element-with-indexof.js
@@ -1,16 +1,14 @@
 function quickCheck(arr, elem) {
   // Only change code below this line
-
-    for(let i = 0; i <= arr.length; i++) {
-        if(arr.indexOf(elem) === arr[i] || arr.indexOf(elem) === i) {
-            return true;
-        } else {
-            return false;
-        }
+  
+    if(arr.indexOf(elem) === -1) {
+      return false;
+    } else {
+      return true;
     }
-    
-    
+  
+  return arr;
   // Only change code above this line
 }
 
-console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushroom'));
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
