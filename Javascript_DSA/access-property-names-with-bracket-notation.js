3fdd68cbc7b0181b57764516680d2c438a31a70f
DaryllCulas <culasdaryll99@gmail.com>
2023-09-03 23:26:37 +0800

    Add access-property-names-with-bracket-notation.js

---
+++ Javascript_DSA/access-property-names-with-bracket-notation.js
@@ -0,0 +1,17 @@
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  // Only change code below this line
+  let inventory = foods[scannedItem];
+  return inventory;
+  // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
